9ecf3a771a1497409d4267c84f6790227fd5b9e3
Filip Pucoski <filip.pucoski@gmail.com>
2020-08-03 17:49:34 +0200

    End of section 10

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -26,7 +26,13 @@ import { UserService } from './_services/user.service';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import {MemberDetailResolver} from './_resolvers/member-detail.resolver';
 import {MemberListResolver} from './_resolvers/member-list.resolver';
+import {MemberEditResolver} from './_resolvers/member-edit.resolver';
+
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+
 import {NgxGalleryModule} from 'ngx-gallery-9';
+import { AuthGuard } from './_guards/auth.guard';
+import {PreventUnsavedChanges} from './_guards/prevent-unsaved-changes.guard';
 
 
 export function tokenGetter() {
@@ -45,7 +51,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -68,8 +75,11 @@ export function tokenGetter() {
       AuthService,
       UserService,
       ErrorInterceptorProvider,
+      AuthGuard,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
--- DatingApp.api/DTOs/PhotosForDetailedDto.cs
+++ DatingApp.api/DTOs/PhotosForDetailedDto.cs
@@ -6,7 +6,7 @@ namespace DatingApp.api.DTOs
     {
         public int Id { get; set; }
         public string Url { get; set; }
-        public string Description { get; set; }
+        public string Introduction { get; set; }
         public DateTime DateAdded { get; set; }
         public bool isMain { get; set; }
      }
